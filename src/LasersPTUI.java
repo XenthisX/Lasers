42e66aa20d9c3c6eb3764cdb74e0aef18dda4008
stm4083 <stm4083@cs.rit.edu>
2016-04-06 12:21:43 -0400

    Fixing merge

--- src/LasersPTUI.java
+++ src/LasersPTUI.java
@@ -1,5 +1,6 @@
 public class LasersPTUI {
     public static void main(String[] args) {
+        System.out.println("My name is Stefan Marchhart");
         System.out.println("My name is Elijah Bosley");
         System.out.println("Our project account is p142-05l");
     }
