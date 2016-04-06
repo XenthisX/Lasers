5c1e2587ee8d79f4ad745ac861ca9f441fb6b7c5
stm4083 <stm4083@cs.rit.edu>
2016-04-06 12:10:05 -0400

    Inital commit

---
+++ src/LasersPTUI.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Stefan on 4/6/2016.
+ */
+public class LasersPTUI {
+    public static void main(String[] args) {
+        System.out.println("My name is Stefan Marchhart");
+        System.out.println("Our project account is p142-05l");
+    }
+}
