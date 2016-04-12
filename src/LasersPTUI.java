146785234607e7a63b780f3af26b1e63f1c9718f
Eli Bosley <ekbosley@gmail.com>
2016-04-12 19:38:35 -0400

    Code cleanup / debugging
    removed try/catch around the scanner for input on top method as well as removing a duplicate scanner, also added some comments to explain what is going on

--- src/LasersPTUI.java
+++ src/LasersPTUI.java
@@ -4,19 +4,31 @@ import java.util.Scanner;
 
 public class LasersPTUI {
 
-    /** an empty cell */
+    /**
+     * an empty cell
+     */
     public final static char EMPTY = '.';
-    /** a cell occupied with a pillar that accepts any # of lasers*/
+    /**
+     * a cell occupied with a pillar that accepts any # of lasers
+     */
     public final static char ANYPILLAR = 'X';
-    /** a cell occupied with a laser emitter*/
+    /**
+     * a cell occupied with a laser emitter
+     */
     public final static char LASER = 'L';
-    /** a cell occupied with a laser beam*/
+    /**
+     * a cell occupied with a laser beam
+     */
     public final static char BEAM = '*';
 
     // OUTPUT CONSTANTS
-    /** A horizontal divider */
+    /**
+     * A horizontal divider
+     */
     public final static char HORI_DIVIDE = '-';
-    /** A vertical divider */
+    /**
+     * A vertical divider
+     */
     public final static char VERT_DIVIDE = '|';
 
     private char[][] grid;
@@ -25,21 +37,24 @@ public class LasersPTUI {
     private int curRow;
     private int curCol;
 
+    /**
+     * First constructor, creates a safe object when only given a safe, with no laser placements
+     *
+     * @param safeFile the safe file to parse in
+     * @throws FileNotFoundException
+     */
     public LasersPTUI(String safeFile) throws FileNotFoundException {
-        try {
-            Scanner in = new Scanner(new File(safeFile));
-        } catch (FileNotFoundException e) {
-            throw e;
-        }
+
         Scanner in = new Scanner(new File(safeFile));
-        width=Integer.parseInt(in.next());
-        height=Integer.parseInt(in.next());
 
-        grid=new char[height][width];
+        width = Integer.parseInt(in.next());
+        height = Integer.parseInt(in.next());
 
-        for (int row=0;row<height;row++){
-            for (int col=0; col<width;col++){
-                grid[row][col]=in.next().charAt(0);
+        grid = new char[height][width];
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                grid[row][col] = in.next().charAt(0);
             }
         }
 
@@ -64,10 +79,10 @@ public class LasersPTUI {
                 break;
             case 1:
 
-                LasersPTUI lasers=new LasersPTUI(args[0]);
+                LasersPTUI lasers = new LasersPTUI(args[0]);
                 break;
             case 2:
-                LasersPTUI lazers=new LasersPTUI(args[0],args[1]);
+                LasersPTUI lazers = new LasersPTUI(args[0], args[1]);
                 break;
         }
         System.out.println("My name is Stefan Marchhart");
